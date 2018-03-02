a6a05f3bc6d82e2ff1d8ff726e18d0c6e50dd8dd
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-03-02 15:04:37 +0800

    refactor: import egg-mongoose

---
+++ app/model/reply.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const BaseModel = require('../common/base_model');
+
+module.exports = app => {
+  const mongoose = app.mongoose;
+  const Schema = mongoose.Schema;
+  const ObjectId = Schema.ObjectId;
+
+  const ReplySchema = new Schema({
+    content: { type: String },
+    topic_id: { type: ObjectId },
+    author_id: { type: ObjectId },
+    reply_id: { type: ObjectId },
+    create_at: { type: Date, default: Date.now },
+    update_at: { type: Date, default: Date.now },
+    content_is_html: { type: Boolean },
+    ups: [ Schema.Types.ObjectId ],
+    deleted: { type: Boolean, default: false },
+  });
+
+  ReplySchema.plugin(BaseModel);
+  ReplySchema.index({ topic_id: 1 });
+  ReplySchema.index({ author_id: 1, create_at: -1 });
+
+  return mongoose.model('Reply', ReplySchema);
+};
--- config/config.local.js
+++ config/config.local.js
@@ -8,3 +8,13 @@ exports.redis = {
     db: 0,
   },
 };
+
+/**
+ * @see http://mongodb.github.io/node-mongodb-native/2.2/api/Db.html#createCollection
+ */
+exports.mongoose = {
+  url: 'mongodb://127.0.0.1/node_club_test',
+  options: {
+    server: { poolSize: 20 },
+  },
+};
