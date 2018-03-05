48453a783c3c493312fafb7fcb952c892c4d05b0
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-03-05 16:59:21 +0800

    feat: add passport support

--- app.js
+++ app.js
@@ -1,10 +1,82 @@
 'use strict';
 
+const tools = require('./app/common/tools');
+
 module.exports = app => {
   if (app.config.debug) {
     app.config.coreMiddleware.unshift('less');
   }
 
-  app.beforeStart(async () => {
+  app.passport.verify(async (ctx, user) => {
+    ctx.logger.debug('passport.verify', user);
+
+    const localHandler = async ({ username, password }) => {
+      const getUser = username => {
+        if (username.indexOf('@') > 0) {
+          return ctx.service.user.getUserByMail(username);
+        }
+        return ctx.service.user.getUserByLoginName(username);
+      };
+      const existUser = await getUser(username);
+
+      // 用户不存在
+      if (!existUser) {
+        return null;
+      }
+
+      const passhash = user.pass;
+      const equal = tools.compare(passhash, password);
+
+      // 密码不匹配
+      if (!equal) {
+        return null;
+      }
+
+      // 用户未激活
+      if (!existUser.active) {
+        // 发送激活邮件
+        return null;
+      }
+
+      // 验证通过
+      return existUser;
+    };
+
+    const githubHandler = async ({ profile }) => {
+      const email = profile.emails && profile.emails[0] && profile.emails[0].value;
+      const existUser = await ctx.service.user.getUsersByQuery({
+        githubId: profile.id,
+      });
+
+      // 用户不存在则创建
+      // TODO
+      if (!existUser) {
+        return null;
+      }
+
+      // 用户存在，更新字段
+      // existUser.loginname = profile.username;
+      existUser.githubId = profile.id;
+      existUser.email = email || existUser.email;
+      existUser.avatar = profile._json.avatar_url;
+      existUser.githubUsername = profile.username;
+      existUser.githubAccessToken = profile.accessToken;
+      await existUser.save();
+
+      return existUser;
+    };
+
+    const handle = user => {
+      let handler;
+      switch (user.provider) {
+        case 'github':
+          handler = githubHandler;
+          break;
+        default:
+          handler = localHandler;
+      }
+      return handler(user);
+    };
+    return handle(user);
   });
 };
--- config/plugin.js
+++ config/plugin.js
@@ -17,3 +17,18 @@ exports.mongoose = {
   enable: true,
   package: 'egg-mongoose',
 };
+
+exports.passport = {
+  enable: true,
+  package: 'egg-passport',
+};
+
+exports.passportGithub = {
+  enable: true,
+  package: 'egg-passport-github',
+};
+
+exports.passportLocal = {
+  enable: true,
+  package: 'egg-passport-local',
+};
