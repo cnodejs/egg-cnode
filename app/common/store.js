af88d85696f6ef879c90f0bd9a4b0ea0a88dc30a
xiaomuzhu <meituandxy@gmail.com>
2018-03-07 22:00:01 +0800

    add promisify qn module

---
+++ app/common/store.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const promisify = require('util').promisify;
+const qn = require('qn');
+const qn_access = require('../../config/config.default').qn_access;
+
+// 7牛 client
+let qnClient = null;
+if (qn_access && qn_access.secretKey !== 'your secret key') {
+  qnClient = qn.create(qn_access);
+}
+
+const qnUpload = promisify(qnClient.upload);
+
+module.exports = qnUpload;
