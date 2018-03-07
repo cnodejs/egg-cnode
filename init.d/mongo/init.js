437096dcb9aee4a5f7817941ee00c612276eadfd
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-03-07 10:01:24 +0800

    chore: restore eslint conf & ignore by comments

--- init.d/mongo/init.js
+++ init.d/mongo/init.js
@@ -1,3 +1,9 @@
+/* eslint-disable */
+
+/**
+ * 1. create custom user
+ * 2. create collection (Before MongoDB can save your new database, a collection name must also be specified at the time of creation.)
+ */
 db.createUser({
   user: 'egg_cnode',
   pwd: 'egg_cnode',
@@ -10,5 +16,5 @@ db.createUser({
 })
 
 db.egg_cnode.insert({
-  'cnode': 'egg-cnode'
+  egg_cnode: 'egg-cnode'
 })
