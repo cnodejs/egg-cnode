cf796ee2fb87a3b7264faa324af66f3c59cc7e38
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-03-08 22:59:21 +0800

    chore: use env as default config

--- config/config.default.js
+++ config/config.default.js
@@ -72,5 +72,36 @@ module.exports = appInfo => {
     ADMIN_USER: true,
   };
 
+  // database
+  config.redis = {
+    client: {
+      host: process.env.EGG_REDIS_HOST || '127.0.0.1',
+      port: process.env.EGG_REDIS_PORT || 6379,
+      password: process.env.EGG_REDIS_PASSWORD || '',
+      db: process.env.EGG_REDIS_DB || '0',
+    },
+  };
+
+  /**
+   * @see http://mongodb.github.io/node-mongodb-native/2.2/api/Db.html#createCollection
+   */
+  config.mongoose = {
+    url: process.env.EGG_MONGODB_URL || 'mongodb://127.0.0.1:27017/egg_cnode',
+    options: {
+      server: { poolSize: 20 },
+    },
+  };
+
+  // passport
+  config.passportGithub = {
+    key: process.env.EGG_PASSPORT_GITHUB_CLIENT_ID || 'test',
+    secret: process.env.EGG_PASSPORT_GITHUB_CLIENT_SECRET || 'test',
+  };
+
+  config.passportLocal = {
+    usernameField: 'name',
+    passwordField: 'pass',
+  };
+
   return config;
 };
