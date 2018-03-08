078c27ae58e95fe215a7fd37b0f8e4f18cd76d24
xiaomuzhu <meituandxy@gmail.com>
2018-03-08 17:01:50 +0800

    fix: user/admin middleware eslint

--- app/middleware/admin_required.js
+++ app/middleware/admin_required.js
@@ -1,22 +1,18 @@
 'use strict';
 
-module.exports = (options, app) => {
-
+module.exports = () => {
   /**
    * 需要管理员权限
    */
+
   return async function(ctx, next) {
     if (!ctx.session.user) {
-      await ctx.render('notify/notify', {
-        error: '你还没有登录。',
-      });
+      await ctx.render('notify/notify', { error: '你还没有登录。' });
       return;
     }
 
     if (!ctx.session.user.is_admin) {
-      await ctx.render('notify/notify', {
-        error: '需要管理员权限。',
-      });
+      await ctx.render('notify/notify', { error: '需要管理员权限。' });
       return;
     }
 
