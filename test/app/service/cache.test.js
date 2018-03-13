dd84a8daea083eb0e2359ab6f4ccb1034dc4a6b0
forl <forl.yang@gmail.com>
2018-03-13 23:19:26 +0800

    feat: add cache.incr

--- test/app/service/cache.test.js
+++ test/app/service/cache.test.js
@@ -16,4 +16,23 @@ describe('test/app/service/cache.test.js', () => {
     const result2 = await ctx.service.cache.get('greet123');
     assert(result2 === undefined);
   });
+
+  it('incr should ok', async () => {
+    const ctx = app.mockContext();
+    const key1 = '' + Date.now();
+
+    let initNumber = await ctx.service.cache.incr(key1, 4);
+    assert(initNumber === 1);
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    let secondNumber = await ctx.service.cache.incr(key1, 1);
+    assert(secondNumber === 2);
+
+    // test expire works
+    const key2 = '' + Date.now();
+    initNumber = await ctx.service.cache.incr(key2, 2);
+    assert(initNumber === 1);
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    secondNumber = await ctx.service.cache.incr(key2, 1);
+    assert(secondNumber === 1);
+  });
 });
