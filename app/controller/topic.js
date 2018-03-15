60604f64b090929ee31d378b5750c8e06d76236b
ngot <zhuanghengfei@gmail.com>
2018-03-15 16:44:05 +0800

    refactor: move qn util to helper

--- app/controller/topic.js
+++ app/controller/topic.js
@@ -7,8 +7,6 @@ const fs = require('fs');
 const awaitWriteStream = require('await-stream-ready').write;
 const sendToWormhole = require('stream-wormhole');
 
-const qnUpload = require('../common/upload');
-
 class TopicController extends Controller {
   /**
    * Topic page
@@ -427,7 +425,7 @@ class TopicController extends Controller {
     // 如果有七牛云的配置,优先上传七牛云
     if (config.qn_access) {
       try {
-        const upload = qnUpload(config.qn_access);
+        const upload = this.ctx.helper.qnUpload(config.qn_access);
         const result = await upload(stream);
         ctx.body = {
           success: true,
