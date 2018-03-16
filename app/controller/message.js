207aef7dabd915677e39cbd36eb2b303784d4758
sinchang <sinchangwen@gmail.com>
2018-03-16 11:26:52 +0800

    chore: improve

--- app/controller/message.js
+++ app/controller/message.js
@@ -6,8 +6,10 @@ class MessageController extends Controller {
   async index() {
     const { ctx } = this;
     const userId = ctx.user._id;
-    const readMessageResults = await ctx.service.message.getReadMessagesByUserId(userId);
-    const unReadMessageResults = await ctx.service.message.getUnreadMessagesByUserId(userId);
+    const [ readMessageResults, unReadMessageResults ] = await Promise.all([
+      ctx.service.message.getReadMessagesByUserId(userId),
+      ctx.service.message.getUnreadMessagesByUserId(userId),
+    ]);
     const hasReadMessages = await Promise.all(readMessageResults.map(async message => await ctx.service.message.getMessageRelations(message)));
     const hasUnReadMessages = await Promise.all(unReadMessageResults.map(async message => await ctx.service.message.getMessageRelations(message)));
 
