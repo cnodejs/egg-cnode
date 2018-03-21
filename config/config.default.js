a8fefced91333c4cd30575d1d38aced375addb09
Qi Yu <njuyuqi@gmail.com>
2018-03-21 10:24:34 +0800

    fix: favicon path

--- config/config.default.js
+++ config/config.default.js
@@ -10,6 +10,8 @@ module.exports = appInfo => {
 
   config.site_logo = '/public/images/cnodejs_light.svg';
 
+  config.site_icon = '/public/images/cnode_icon_32.png';
+
   // debug 为 true 时，用于本地调试
   config.debug = true;
 
