af01c539612f55efacc340c074fa37b0460f6660
sinchang <sinchangwen@gmail.com>
2018-05-10 13:51:49 +0800

    chore: update get accesstoken

--- app/middleware/token_required.js
+++ app/middleware/token_required.js
@@ -4,13 +4,11 @@ module.exports = () => {
   return async function(ctx, next) {
     let token = '';
     if (
-      ctx.headers.authorization &&
-      (ctx.headers.authorization.split(' ')[0] === 'Bearer' ||
-      ctx.headers.authorization.split(' ')[0] === 'Token')
+      ctx.headers.authorization && ctx.headers.authorization.split(' ')[0] === 'Bearer'
     ) {
       token = ctx.headers.authorization.split(' ')[1];
-    } else if (ctx.query.token) {
-      token = ctx.query.token;
+    } else if (ctx.query.accesstoken) {
+      token = ctx.query.accesstoken;
     } else if (ctx.request.body.accesstoken) {
       token = ctx.request.body.accesstoken;
     }
